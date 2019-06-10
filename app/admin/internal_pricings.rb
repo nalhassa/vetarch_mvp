d088acfcda4e7e03a65865904f7e1134910c3675
Raghu Betina <raghu@firstdraft.com>
2019-06-10 18:16:44 +0000

    Whitelist columns in InternalPricing for ActiveAdmin

--- app/admin/internal_pricings.rb
+++ app/admin/internal_pricings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register InternalPricing do
+
+ permit_params :cet, :price, :cogs, :medication_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
