2ae964c2d74eda8daad72f591c56dc1349d6295a
Raghu Betina <raghu@firstdraft.com>
2019-06-10 18:17:34 +0000

    Only prescription user can modify prescription

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,4 +1,14 @@
 class PrescriptionsController < ApplicationController
+  before_action :current_user_must_be_prescription_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_prescription_user
+    prescription = Prescription.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == prescription.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @prescriptions = Prescription.all
 
