8ca1b4d2324f56aadda3de8e49478a9cd0be8bb6
Raghu Betina <raghu@firstdraft.com>
2019-06-10 18:16:16 +0000

    Whitelist columns in Client for ActiveAdmin

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Client do
+
+ permit_params :first_name, :last_name, :street_address, :zip_code, :apartment_number
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
