3ce2c09beb157e10e92124af8c2155a39b36bc58
Raghu Betina <raghu@firstdraft.com>
2019-06-10 18:16:38 +0000

    Whitelist columns in OnlinePricing for ActiveAdmin

--- app/admin/online_pricings.rb
+++ app/admin/online_pricings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register OnlinePricing do
+
+ permit_params :provider, :price, :ppd, :hyperlink, :medication_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
